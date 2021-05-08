f2b305676428599531dc3033a9eb148ab56d1114
kamogelo phogole <phogolekk@gmail.com>
2021-05-08 18:09:01 +0200

    first commit

---
+++ src/main/java/org/sars/hrsystem/HrsystemApplication.java
@@ -0,0 +1,13 @@
+package org.sars.hrsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HrsystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HrsystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/sars/hrsystem/HrsystemApplicationTests.java
@@ -0,0 +1,13 @@
+package org.sars.hrsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrsystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
